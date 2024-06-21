b1efe8dc690f4e48d3a000572fef17af143614fb
Marlin <hello@marlin.red>
2024-06-21 18:51:22 +0800

    Initial Commit

---
+++ Neno/NenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NenoApp.swift
+//  Neno
+//
+//  Created by Drawix on 2024/6/21.
+//
+
+import SwiftUI
+
+@main
+struct NenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
